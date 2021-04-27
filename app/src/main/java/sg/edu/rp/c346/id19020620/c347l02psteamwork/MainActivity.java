dd5d679ccdbaa16d0ec513e2d806dd5c7abda2b6
19020620 <19020620@myrp.edu.sg>
2021-04-27 12:12:07 +0800

    first activity

--- app/src/main/java/sg/edu/rp/c346/id19020620/c347l02psteamwork/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19020620/c347l02psteamwork/MainActivity.java
@@ -3,12 +3,27 @@ package sg.edu.rp.c346.id19020620.c347l02psteamwork;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView lv;
+    ArrayList<String> al;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv = (ListView) this.findViewById(R.id.lvModules);
+        al = new ArrayList<String>();
+        al.add("C347");
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, al);
+        lv.setAdapter(adapter);
     }
 }
